5036e9f24ed2a4c8156747006610adfaf51382ba
nicoantonucci <nicoantonucci2@gmail.com>
2020-11-13 10:05:57 -0300

    Delete index.js

--- src/store/sagas/index.js
+++
@@ -1,9 +0,0 @@
-import { takeEvery } from 'redux-saga/effects';
-
-import * as actionTypes from './../actionTypes';
-
-import { initGnomesSaga } from './gnomes';
-
-export function* watchGnomes() {
-  yield takeEvery(actionTypes.INIT_GNOMES, initGnomesSaga);
-}
